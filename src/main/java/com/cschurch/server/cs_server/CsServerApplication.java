4b6cc1bc7029d9e5e3ed0a6bc8bb89c2d7acdb6e
seunghyunyyy <seunghyunyyy@kakao.com>
2022-12-27 10:40:52 +0900

    Setting

---
+++ src/main/java/com/cschurch/server/cs_server/CsServerApplication.java
@@ -0,0 +1,13 @@
+package com.cschurch.server.cs_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsServerApplication.class, args);
+	}
+
+}
